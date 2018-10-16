6c6261f7e633a072ad08e137bf6996fc2b62393b
root <alienblog@outlook.com>
2018-10-16 17:05:55 +0900

    update

--- calil.sh
+++ calil.sh
@@ -9,18 +9,14 @@ do
 	(cd $i;git pull origin master)
 done
 
-rm -rf env
-virtualenv env
-source env/bin/activate
 (cd genpac;python setup.py install)
 
-env/bin/genpac \
+genpac \
 	--pac-proxy "SOCKS5 127.0.0.1:1081" \
 	--gfwlist-url - \
 	--gfwlist-local gfwlist/gfwlist.txt \
 	-o gfwlist.pac
 sed -e '5d' -e '3d' -i gfwlist.pac
-deactivate
 
 git add .
 git commit -m "[$(LANG=C date)]auto update"
